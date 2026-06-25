04bab554ec379d898ebdff12bd08f185edf747e7
piku-2 <207675780+piku-2@users.noreply.github.com>
2026-06-25 22:00:04 +0900

    sitemap robots add

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
-    site: 'https://jyousho-official-site.pages.dev'
+  site: 'https://jyousho-official-site.pages.dev',
+  integrations: [sitemap()]
 });
